c07f9c4bc4bc131923f1e7609e568b69dfb67728
Aurélien <107919117+Issei1801@users.noreply.github.com>
2023-08-28 16:12:22 +0200

    Ajout des crud pour catégorie et produits

---
+++ src/Controller/Admin/ProduitsCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Produits;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ProduitsCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Produits::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            NumberField::new('prix'),
+            TextareaField::new('description'),
+            AssociationField::new('categorie', 'Categorie')->autocomplete()
+            ->setCrudController(CategorieCrudController::class),
+            ImageField::new('image')->setUploadDir('public/assets/images/produits')
+        ];
+    }
+}
