9366774e06ed29c6cae9845c4368f622b0c4383e
Aurélien <107919117+Issei1801@users.noreply.github.com>
2023-08-28 16:09:56 +0200

    ✨ Création page d'accueil

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategorieRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'accueil')]
+    public function index(
+        CategorieRepository $categorie,
+    ): Response
+    {
+        return $this->render('homepage/accueil.html.twig', [
+            'controller_name' => 'HomepageController',
+            'categorie' => $categorie->findAll(),
+        ]);
+    }
+}
