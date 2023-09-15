50c93f8c765ac56a3aeffd8ce67edf003621419a
Aurélien <107919117+Issei1801@users.noreply.github.com>
2023-09-15 14:05:43 +0200

    🔨 💄 ✨ Amélioration du site

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,18 +4,29 @@ namespace App\Controller\Admin;
 
 use App\Entity\Produits;
 use App\Entity\Categorie;
+use Symfony\UX\Chartjs\Model\Chart;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(
+        private ChartBuilderInterface $chartBuilder,
+    ) {
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+
+        return $this->render('admin/dashboard.html.twig', [
+            'chart' => $chart,
+        ]);
     }
 
     public function configureDashboard(): Dashboard
